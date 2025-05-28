b63629fd54493661e3d693b2b1dabaefdccf0ba0
tiae24 <tiae24@student.bth.se>
2025-05-28 02:41:03 +0200

    Trying to fix scrutinizer

--- tests/Controller/ApiControllerTest.php
+++ tests/Controller/ApiControllerTest.php
@@ -23,7 +23,32 @@ class ApiControllerTest extends WebTestCase
     }
 
 
+    public function testShowAllProduct(): void
+    {
+        // This calls KernelTestCase::bootKernel(), and creates a
+        // "client" that is acting as the browser
+        $client = static::createClient();
 
+        // Request a specific page
+        $crawler = $client->request('GET', 'api/library/books');
+
+        // Validate a successful response and some content
+        $this->assertResponseIsSuccessful();
+    }
+
+
+    public function testShowBookByISBN(): void
+    {
+        // This calls KernelTestCase::bootKernel(), and creates a
+        // "client" that is acting as the browser
+        $client = static::createClient();
+
+        // Request a specific page
+        $crawler = $client->request('GET', 'api/library/book/9780795300622');
+
+        // Validate a successful response and some content
+        $this->assertResponseIsSuccessful();
+    }
 
 
 
